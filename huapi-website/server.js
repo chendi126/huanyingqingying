2187fff683821c422aa7891d8b55c9b7f7d3e5fd
Test User <test@example.com>
2025-10-20 19:17:45 +0800

    Fix Express routing for SPA - use app.use instead of app.get

--- huapi-website/server.js
+++ huapi-website/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(join(__dirname, 'dist')));
 
 // 处理 SPA 路由 - 所有请求都返回 index.html
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(join(__dirname, 'dist', 'index.html'));
 });
 
